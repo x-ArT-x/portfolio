0952bc7e22a2ffbe251b55a92fee3b87942b07c2
x-ArT-x <56474478+x-ArT-x@users.noreply.github.com>
2020-06-13 19:00:36 +0300

    change7

---
+++ flippers/js/flippers.js
@@ -0,0 +1,8 @@
+function ChangeImageBambuk(){
+document.getElementById('bamboo').classList.remove("DisacivateImage");
+document.getElementById('al').classList.add("DisacivateImage");
+}
+function ChangeImageAlu(){
+
+
+}
