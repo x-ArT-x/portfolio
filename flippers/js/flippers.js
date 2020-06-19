ced73068e2f11dcbcf65c1e123c444d923e854f1
x-ArT-x <56474478+x-ArT-x@users.noreply.github.com>
2020-06-19 13:21:47 +0300

    change7

--- flippers/js/flippers.js
+++ flippers/js/flippers.js
@@ -1,8 +1,12 @@
 function ChangeImageBambuk(){
-document.getElementById('bamboo').classList.remove("DisacivateImage");
-document.getElementById('al').classList.add("DisacivateImage");
+document.getElementById('bamboo').classList.remove("DisactivateImage");
+document.getElementById('al').classList.add("DisactivateImage");
+document.getElementById('but1').classList.remove("activeButton");
+document.getElementById('but2').classList.add("activeButton");
 }
 function ChangeImageAlu(){
-
-
+document.getElementById('al').classList.remove("DisactivateImage");
+document.getElementById('bamboo').classList.add("DisactivateImage");
+document.getElementById('but2').classList.remove("activeButton");
+document.getElementById('but1').classList.add("activeButton");
 }
